edcb6fc3a4eebecc5bee6575bb8483a7156f6e2f
unknown <seancollins117@gmail.com>
2022-01-31 06:33:12 -0800

    another big database leap

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
 CREATE TABLE departments (
-id INTEGER PRIMARY KEY,
+id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
